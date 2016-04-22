08405517d76da7b108395e30f44242005adffccd
Mysticmusti <bosmansnico@hotmail.com>
2016-04-22 15:48:56 +0200

    Clock

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,21 @@ var app = angular.module('QuestLogger', []);
 app.controller('yearC', function ($scope) {
     $scope.cDate = new Date().getFullYear(); 
 });
+
+function MyController($scope) {
+  $scope.clock = {
+    clock : new Date()
+  }
+    var updateClock = function () {
+        $scope.clock.now = new Date();
+    };
+
+    setInterval(function () {
+        $scope.$apply(updateClock);
+    }, 1000);
+
+    updateClock();
+}
 /*ToDoList
 ------------------*/
 app.controller('toDoC', function ($scope) {
